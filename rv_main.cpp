e31e98123972e13991aa236fb52aff32dbd153b6
lxf <leon.rosenfeild@gmail.com>
2022-02-03 11:10:18 -0500

    updates

--- rv_main.cpp
+++ rv_main.cpp
@@ -28,7 +28,7 @@ static const char *auth, *serv;
 static uint16_t port;
 float ds18b20(float results[]);
 int readADC(float results[]);
-
+float fudge = 1/.1747;
 #include <BlynkWidgets.h>
 
 BlynkTimer tmr;
@@ -38,7 +38,7 @@ void getCPUTemperature(float *v){// sub function used to print CPU temperature
     float CPU_temp;
     // CPU temperature data is stored in this directory.
     fp=fopen("/sys/class/thermal/thermal_zone0/temp","r");
-    fgets(str_temp,15,fp);      // read file temp
+    fgets(str_temp,15,fp);        // read file temp
     *v = atof(str_temp)/1000.0;   // convert to Celsius degrees
     fclose(fp);
 }
@@ -46,18 +46,45 @@ void getCPUTemperature(float *v){// sub function used to print CPU temperature
 BLYNK_WRITE(V0)
 {
 
-    int chk=-1;
     DHT dht;			//create a DHT class object
+    int chk;
     chk = dht.readDHT11(DHT11_Pin);
-    if(chk == DHTLIB_OK){
+    if(chk == DHTLIB_OK)
     	Blynk.virtualWrite(V6, dht.temperature * 9.0 / 5.0 + 32.0); 
-    	float v,results[4] ;
-    	getCPUTemperature(&v);
-    	Blynk.virtualWrite(V1,  v); 
-    	readADC(results);
-    	Blynk.virtualWrite(V2, results[0]);
-    	Blynk.virtualWrite(V3, results[1]);
-   }
+
+    float v,results[4] ;
+    getCPUTemperature(&v);
+    Blynk.virtualWrite(V1,  v); 
+
+    readADC(results);
+    Blynk.virtualWrite(V2, results[0]*fudge);
+    Blynk.virtualWrite(V3, results[1]);
+    Blynk.virtualWrite(V5, fudge);
+    printf("fudge %f\n",fudge);
+}
+BLYNK_WRITE(V5)
+{
+	fudge = param.asFloat();
+}
+
+BLYNK_WRITE(V4)
+{
+	fudge = 1/.1747;
+    	Blynk.virtualWrite(V5, fudge);
+
+
+}
+BLYNK_WRITE(V7)
+{
+	  switch (param.asInt())
+  {
+    case 1: // Item 1
+      printf("Item 1 selected\n");
+      break;
+    case 2: // Item 2
+      printf("Item 2 selected\n");
+      break;
+  }
 }
 
 void setup()
