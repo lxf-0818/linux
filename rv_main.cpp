5467594d7f19ca68bc9a7d6a9d513f208d3763c1
lxf <leon.rosenfeild@gmail.com>
2022-02-03 18:56:11 -0500

    updates

--- rv_main.cpp
+++ rv_main.cpp
@@ -24,11 +24,12 @@
 static BlynkTransportSocket _blynkTransport;
 BlynkSocket Blynk(_blynkTransport);
 #define DHT11_Pin  22		//define the pin of sensor
+#define FUDGE 1/.1747		//Voltage Divide Resistance 
 static const char *auth, *serv;
 static uint16_t port;
 float ds18b20(float results[]);
 int readADC(float results[]);
-float fudge = 1/.1747;
+float fudge = FUDGE;
 #include <BlynkWidgets.h>
 
 BlynkTimer tmr;
@@ -60,31 +61,18 @@ BLYNK_WRITE(V0)
     Blynk.virtualWrite(V2, results[0]*fudge);
     Blynk.virtualWrite(V3, results[1]);
     Blynk.virtualWrite(V5, fudge);
-    printf("fudge %f\n",fudge);
 }
 BLYNK_WRITE(V5)
 {
+	//Read Custom fudge and apply
 	fudge = param.asFloat();
 }
 
 BLYNK_WRITE(V4)
 {
-	fudge = 1/.1747;
+	//reset 
+	fudge = FUDGE;
     	Blynk.virtualWrite(V5, fudge);
-
-
-}
-BLYNK_WRITE(V7)
-{
-	  switch (param.asInt())
-  {
-    case 1: // Item 1
-      printf("Item 1 selected\n");
-      break;
-    case 2: // Item 2
-      printf("Item 2 selected\n");
-      break;
-  }
 }
 
 void setup()
