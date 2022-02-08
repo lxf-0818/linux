62a33643620cc8de0d95c5cedf78f0e5ba5d516d
lxf <leon.rosenfeild@gmail.com>
2022-02-07 19:07:35 -0500

    updates

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-	echo $1
+if [ $# -eq 0 ]
+	then
+    		cho "No arguments supplied"
+    		it 0 
+fi
+if [ -f "$1" ]; then
     make clean all target=raspberry file=$1
     rm *.o
     exit 0
+else
+    echo "$1 does not exists."
+
+fi
 exit 1
